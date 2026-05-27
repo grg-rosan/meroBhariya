7be10e7a43f46957805e8a216cb40ff1e00eae12
grg-rosan <grg.rosan99@gmail.com>
2026-05-27 17:35:57 +0545

    fixed missing tls of redis

--- server/src/config/redis.config.js
+++ server/src/config/redis.config.js
@@ -1,15 +1,19 @@
 import { createClient } from "redis";
 import logger from "../infrastructure/logger/index.js";
+
 let client;
 
 export async function getRedisClient() {
   if (client?.isOpen) return client;
 
+  const isTLS = process.env.REDIS_URL?.startsWith("rediss://");
+
   client = createClient({
     url: process.env.REDIS_URL,
     socket: {
-      rejectUnauthorized: false
-    }
+      tls: isTLS,         
+      rejectUnauthorized: false,
+    },
   });
 
   client.on("error", (err) => logger.error({ err }, "[Redis]"));
